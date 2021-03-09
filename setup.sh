175face4628e1e46f1bd8267b19aebe56f077dc9
Prashant Gupta <52120873+prashantgupta8feb@users.noreply.github.com>
2021-03-09 23:32:30 +0530

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,7 +1,7 @@
 mkdir -p ~/.streamlit/
 echo "\
 [general]\n\
-email = \"prashantgupta8feb@gmail.com\"\n\
+email = \"your-email@domain.com\"\n\
 " > ~/.streamlit/credentials.toml
 echo "\
 [server]\n\
